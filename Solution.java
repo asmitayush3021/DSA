2fa035337618b5f6cb7292c20cad3a537a2d38ab
Asmit Kumar Singh <asmit.kumarsingh.cse26@heritageit.edu.in>
2025-12-24 14:44:54 +0530

    Changed

---
+++ Solution.java
@@ -0,0 +1,7 @@
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("First line!");
+        System.out.println("Second line!");
+        System.out.println("Third line!");
+    }
+}
